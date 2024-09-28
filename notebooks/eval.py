100b1a4926da1b6df9bd71ed5e3439c56c7f1df1
KostyaGukish <kostya.gukish@mail.ru>
2024-09-28 19:45:30 +0300

    baseline predictions and fixed feature engineering function

--- notebooks/eval.py
+++ notebooks/eval.py
@@ -7,48 +7,60 @@ from copy import deepcopy
 from prophet import Prophet
 from joblib import Parallel, delayed
 
-def my_cross_validation(estimator, X, y, params, cv):
+
+def fit_predict_prophet(model, x_train, y_train, x_test):
+    df = pd.concat([x_train, y_train], axis=1)
+    df = df.rename(columns={"visit_date": "ds", "visitors": "y"})
+    model.fit(df)
+
+    df = deepcopy(x_test)
+    df = df.rename(columns={"visit_date": "ds"})
+    pred = model.predict(df)
+    pred = pred[["yhat"]]
+    pred[pred < 0] = 0
+    return pred
+
+
+def fit_model(estimator, X, y, params, train_index, test_index):
+    model = estimator(**params)
+    x_train, x_test = X.loc[train_index], X.loc[test_index]
+    y_train, y_test = y.loc[train_index], y.loc[test_index]
+
+    if isinstance(estimator(), Prophet):
+        pred = fit_predict_prophet(model, x_train, y_train, x_test)
+
+    else:
+        model.fit(x_train, y_train)
+
+        pred = model.predict(x_test)
+
+    return root_mean_squared_log_error(y_test, pred)
+
+
+def my_cross_validation(estimator, X, y, params, cv, n_jobs=1):
     scores = []
-    pointer = 0
 
     for train_index, test_index in cv.split(X, y):
-        model = estimator(**params)
-        pointer += 1
-        x_train, x_test = X.loc[train_index], X.loc[test_index]
-        y_train, y_test = y.loc[train_index], y.loc[test_index]
-
-        if isinstance(estimator(), Prophet):
-            df = pd.concat([x_train, y_train], axis=1)
-            df = df.rename(columns={"visit_date": "ds", "visitors": "y"})
-            model.fit(df)
-
-            df = deepcopy(x_test)
-            df = df.rename(columns={"visit_date": "ds"})
-            pred = model.predict(df)
-            pred = pred[["yhat"]]
-            pred[pred < 0] = 0
-
-        else:
-            model.fit(x_train, y_train)
-
-            pred = model.predict(x_test)
-
-        score = root_mean_squared_log_error(y_test, pred)
-
-        scores.append(score)
+        scores.append(fit_model(estimator, X, y, params, train_index, test_index))
 
     model_score = np.mean(scores)
 
     return model_score
 
-def my_grid_search_cv(estimator, X, y, param_grid, cv, n_jobs=1):
+
+def my_grid_search_cv(
+    estimator,
+    X,
+    y,
+    param_grid,
+    cv,
+    n_jobs=1,
+):
     keys, values = zip(*param_grid.items())
     permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
     scores = Parallel(n_jobs=n_jobs)(
-        delayed(my_cross_validation)(
-            estimator=estimator, X=X, y=y, params=params, cv=cv
-        )
+        delayed(my_cross_validation)(estimator, X, y, params, cv, n_jobs)
         for params in permutations_dicts
     )
 
--- restaurants/restaurants/__init__.py
+++
@@ -1,1 +0,0 @@
-from restaurants import config  # noqa: F401
