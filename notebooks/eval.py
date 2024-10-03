e8c559e8e27ed3d5f9f0bc4151803b9a082b78f9
KostyaGukish <kostya.gukish@mail.ru>
2024-10-03 09:22:12 +0300

    month splitter

--- notebooks/eval.py
+++ notebooks/eval.py
@@ -8,6 +8,36 @@ from prophet import Prophet
 from joblib import Parallel, delayed
 
 
+def split_data(data, k, period = "month"):
+    latest_date = data['visit_date'].max()
+    
+    month = latest_date - pd.DateOffset(months=k)
+    next_month = latest_date - pd.DateOffset(months=k-1)
+
+    if period == "week":
+        month = latest_date - pd.DateOffset(weeks=k)
+        next_month = latest_date - pd.DateOffset(weeks=k-1)
+    
+    test = np.array(data[(data['visit_date'] >= month) & (data['visit_date'] < next_month)].index)
+    train = np.array(data[data['visit_date'] < month].index)
+
+    return train, test
+
+
+def split(X):
+    n_validation_months = X['visit_date'].dt.to_period('M').nunique() - 1
+    n_splits = min(n_validation_months, 5)
+
+    if n_validation_months <= 1:
+        for i in range(2, 0, -1):
+            yield split_data(X, i, "week")
+    else:
+        for i in range(n_splits, 0, -1):
+            train, test = split_data(X, i, "month")
+            if train.size > 2 and test.size > 2:
+                yield split_data(X, i, "month")
+
+
 def fit_predict_prophet(model, x_train, y_train, x_test):
     df = pd.concat([x_train, y_train], axis=1)
     df = df.rename(columns={"visit_date": "ds", "visitors": "y"})
@@ -37,10 +67,10 @@ def fit_model(estimator, X, y, params, train_index, test_index):
     return root_mean_squared_log_error(y_test, pred)
 
 
-def my_cross_validation(estimator, X, y, params, cv, n_jobs=1):
+def my_cross_validation(estimator, X, y, params, n_jobs=1):
     scores = []
 
-    for train_index, test_index in cv.split(X, y):
+    for train_index, test_index in split(X):
         scores.append(fit_model(estimator, X, y, params, train_index, test_index))
 
     model_score = np.mean(scores)
@@ -53,14 +83,13 @@ def my_grid_search_cv(
     X,
     y,
     param_grid,
-    cv,
     n_jobs=1,
 ):
     keys, values = zip(*param_grid.items())
     permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
     scores = Parallel(n_jobs=n_jobs)(
-        delayed(my_cross_validation)(estimator, X, y, params, cv, n_jobs)
+        delayed(my_cross_validation)(estimator, X, y, params, n_jobs)
         for params in permutations_dicts
     )
 
